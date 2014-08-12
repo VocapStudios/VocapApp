67c5ce4bfeda91421e267ad06d46162cc4700118
Subin <subjsam@gmail.com>
2014-08-12 14:53:32 -0400

    added a word bank and a video player

---
+++ VocapApp-master/dictionary.js
@@ -0,0 +1,608 @@
+var dictionarySpanishTwo = [
+"abajo",
+"dr",
+"hija",
+"dejar",
+"necesita",
+"llegar",
+"hago",
+"señora",
+"haya",
+"suficiente",
+"doctor",
+"gustaría",
+"tierra",
+"cara",
+"siquiera",
+"genial",
+"cree",
+"supuesto",
+"tomar",
+"equipo",
+"justo",
+"juego",
+"ninguna",
+"matar",
+"cinco",
+"dicen",
+"amo",
+"cuándo",
+"pequeño",
+"algunos",
+"conozco",
+"clase",
+"maldito",
+"unas",
+"muchos",
+"hubiera",
+"segundo",
+"aunque",
+"pueda",
+"dime",
+"igual",
+"comida",
+"ay",
+"cuerpo",
+"encontrar",
+"fuerte",
+"vuelta",
+"venga",
+"creer",
+"realidad",
+"saben",
+"puta",
+"deberías",
+"pregunta",
+"fui",
+"cuatro",
+"sra",
+"primer",
+"trabajo",
+"e",
+"hagas",
+"alto",
+"maldita",
+"comer",
+"número",
+"dar",
+"necesitas",
+"oportunidad",
+"punto",
+"misma",
+"última",
+"afuera",
+"mujeres",
+"pensar",
+"fueron",
+"difícil",
+"vivir",
+"paso",
+"malo",
+"estabas",
+"vivo",
+"haga",
+"queda",
+"hijos",
+"mayor",
+"fiesta",
+"hacen",
+"medio",
+"algunas",
+"basta",
+"arma",
+"vino",
+"meses",
+"cuarto",
+"éste",
+"escuela",
+"esté",
+"dólares",
+"tío",
+"posible"];
+
+var sortedDictionarySpanishTwo = dictionarySpanishTwo.sort();
+//the Dictionary File
+var dictionarySpanish = {
+   "word": [
+      {
+         "abajo": {
+            "translation": "down"
+         }
+      },
+      {
+         "dr": {
+            "translation": "Doctor"
+         }
+      },
+      {
+         "hija": {
+            "translation": "daughter"
+         }
+      },
+      {
+         "dejar": {
+            "translation": "leave"
+         }
+      },
+      {
+         "necesita": {
+            "translation": "need"
+         }
+      },
+      {
+         "llegar": {
+            "translation": "to get"
+         }
+      },
+      {
+         "hago": {
+            "translation": "do"
+         }
+      },
+      {
+         "señora": {
+            "translation": "Mrs."
+         }
+      },
+      {
+         "haya": {
+            "translation": "have been"
+         }
+      },
+      {
+         "suficiente": {
+            "translation": "enough"
+         }
+      },
+      {
+         "doctor": {
+            "translation": "Doctor"
+         }
+      },
+      {
+         "gustaría": {
+            "translation": "like"
+         }
+      },
+      {
+         "tierra": {
+            "translation": "land"
+         }
+      },
+      {
+         "cara": {
+            "translation": "face"
+         }
+      },
+      {
+         "siquiera": {
+            "translation": "even"
+         }
+      },
+      {
+         "genial": {
+            "translation": "great"
+         }
+      },
+      {
+         "cree": {
+            "translation": "believe"
+         }
+      },
+      {
+         "supuesto": {
+            "translation": "assume"
+         }
+      },
+      {
+         "tomar": {
+            "translation": "take"
+         }
+      },
+      {
+         "equipo": {
+            "translation": "computer equip"
+         }
+      },
+      {
+         "justo": {
+            "translation": "just"
+         }
+      },
+      {
+         "juego": {
+            "translation": "game play"
+         }
+      },
+      {
+         "ninguna": {
+            "translation": "none"
+         }
+      },
+      {
+         "matar": {
+            "translation": "kill"
+         }
+      },
+      {
+         "cinco": {
+            "translation": "five"
+         }
+      },
+      {
+         "dicen": {
+            "translation": "say"
+         }
+      },
+      {
+         "amo": {
+            "translation": "love love"
+         }
+      },
+      {
+         "cuándo": {
+            "translation": "when"
+         }
+      },
+      {
+         "pequeño": {
+            "translation": "small"
+         }
+      },
+      {
+         "algunos": {
+            "translation": "any"
+         }
+      },
+      {
+         "conozco": {
+            "translation": "know"
+         }
+      },
+      {
+         "clase": {
+            "translation": "class"
+         }
+      },
+      {
+         "maldito": {
+            "translation": "damn"
+         }
+      },
+      {
+         "unas": {
+            "translation": "one"
+         }
+      },
+      {
+         "muchos": {
+            "translation": "a lot of"
+         }
+      },
+      {
+         "hubiera": {
+            "translation": "have"
+         }
+      },
+      {
+         "segundo": {
+            "translation": "second"
+         }
+      },
+      {
+         "aunque": {
+            "translation": "even if"
+         }
+      },
+      {
+         "pueda": {
+            "translation": "power"
+         }
+      },
+      {
+         "dime": {
+            "translation": "say"
+         }
+      },
+      {
+         "igual": {
+            "translation": "Like"
+         }
+      },
+      {
+         "comida": {
+            "translation": "food to eat"
+         }
+      },
+      {
+         "ay": {
+            "translation": "ay"
+         }
+      },
+      {
+         "cuerpo": {
+            "translation": "body"
+         }
+      },
+      {
+         "encontrar": {
+            "translation": "find"
+         }
+      },
+      {
+         "fuerte": {
+            "translation": "strong"
+         }
+      },
+      {
+         "vuelta": {
+            "translation": "Return back"
+         }
+      },
+      {
+         "venga": {
+            "translation": "come"
+         }
+      },
+      {
+         "creer": {
+            "translation": "believe"
+         }
+      },
+      {
+         "realidad": {
+            "translation": "reality"
+         }
+      },
+      {
+         "saben": {
+            "translation": "know"
+         }
+      },
+      {
+         "puta": {
+            "translation": "whore"
+         }
+      },
+      {
+         "deberías": {
+            "translation": "duty"
+         }
+      },
+      {
+         "pregunta": {
+            "translation": "question asking"
+         }
+      },
+      {
+         "fui": {
+            "translation": "going to be"
+         }
+      },
+      {
+         "cuatro": {
+            "translation": "four"
+         }
+      },
+      {
+         "sra": {
+            "translation": "Mrs."
+         }
+      },
+      {
+         "primer": {
+            "translation": "first"
+         }
+      },
+      {
+         "trabajo": {
+            "translation": "work"
+         }
+      },
+      {
+         "e": {
+            "translation": "and"
+         }
+      },
+      {
+         "hagas": {
+            "translation": "do"
+         }
+      },
+      {
+         "alto": {
+            "translation": "high"
+         }
+      },
+      {
+         "maldita": {
+            "translation": "damn"
+         }
+      },
+      {
+         "comer": {
+            "translation": "eat"
+         }
+      },
+      {
+         "número": {
+            "translation": "number"
+         }
+      },
+      {
+         "dar": {
+            "translation": "give"
+         }
+      },
+      {
+         "necesitas": {
+            "translation": "need"
+         }
+      },
+      {
+         "oportunidad": {
+            "translation": "opportunity"
+         }
+      },
+      {
+         "punto": {
+            "translation": "point"
+         }
+      },
+      {
+         "misma": {
+            "translation": "same"
+         }
+      },
+      {
+         "última": {
+            "translation": "Finally"
+         }
+      },
+      {
+         "afuera": {
+            "translation": "out"
+         }
+      },
+      {
+         "mujeres": {
+            "translation": "Women"
+         }
+      },
+      {
+         "pensar": {
+            "translation": "think"
+         }
+      },
+      {
+         "fueron": {
+            "translation": "going to be"
+         }
+      },
+      {
+         "difícil": {
+            "translation": "difficult"
+         }
+      },
+      {
+         "vivir": {
+            "translation": "live"
+         }
+      },
+      {
+         "paso": {
+            "translation": "step move"
+         }
+      },
+      {
+         "malo": {
+            "translation": "bad"
+         }
+      },
+      {
+         "estabas": {
+            "translation": "be"
+         }
+      },
+      {
+         "vivo": {
+            "translation": "Live Chat"
+         }
+      },
+      {
+         "haga": {
+            "translation": "do"
+         }
+      },
+      {
+         "queda": {
+            "translation": "be"
+         }
+      },
+      {
+         "hijos": {
+            "translation": "son"
+         }
+      },
+      {
+         "mayor": {
+            "translation": "more"
+         }
+      },
+      {
+         "fiesta": {
+            "translation": "party"
+         }
+      },
+      {
+         "hacen": {
+            "translation": "do"
+         }
+      },
+      {
+         "medio": {
+            "translation": "Average"
+         }
+      },
+      {
+         "algunas": {
+            "translation": "any"
+         }
+      },
+      {
+         "basta": {
+            "translation": "sufficient"
+         }
+      },
+      {
+         "arma": {
+            "translation": "gun arm"
+         }
+      },
+      {
+         "vino": {
+            "translation": "come wine"
+         }
+      },
+      {
+         "meses": {
+            "translation": "month"
+         }
+      },
+      {
+         "cuarto": {
+            "translation": "fourth"
+         }
+      },
+      {
+         "éste": {
+            "translation": "this"
+         }
+      },
+      {
+         "escuela": {
+            "translation": "school"
+         }
+      },
+      {
+         "esté": {
+            "translation": "be"
+         }
+      },
+      {
+         "dólares": {
+            "translation": "dollar"
+         }
+      },
+      {
+         "tío": {
+            "translation": "Uncle"
+         }
+      },
+      {
+         "posible": {
+            "translation": "possible"
+         }
+      }
+   ]
+};
